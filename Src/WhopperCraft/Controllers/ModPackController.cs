b81208e4ba5100d6f6408d83c196c8b6fd9f0e65
ThatOneCodeDev <60614943+ThatOneCodeDev@users.noreply.github.com>
2023-05-07 00:07:02 +0200

    Initial Commit

---
+++ Src/WhopperCraft/Controllers/ModPackController.cs
@@ -0,0 +1,30 @@
+﻿// ModPackController.cs
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhopperCraft.Controllers
+{
+    public class ModPackController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Download()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot", "lib", "mods.zip");
+
+            if (!System.IO.File.Exists(file))
+                return NotFound();
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(file, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, "application/octet-stream", Path.GetFileName(file));
+        }
+    }
+
+}
